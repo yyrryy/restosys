ad5fe5cb15fe46b1257b9e43612a47cb9eab270b
Your Name <you@example.com>
2026-05-10 23:38:32 +0100

    'reer'

--- restosys/urls.py
+++ restosys/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from restaurant import views as restaurant_views
 
 urlpatterns = [
@@ -30,7 +31,9 @@ urlpatterns = [
 
 if settings.USE_SQLITE:
     urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
